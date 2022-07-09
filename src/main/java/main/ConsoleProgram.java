c8e1f44ba4d0ef1befbcab3b9d35267a934adb6b
ReCiffer <jonasfrey@outlook.com>
2022-07-09 13:32:57 +0200

    Updated schema

--- src/main/java/main/ConsoleProgram.java
+++ src/main/java/main/ConsoleProgram.java
@@ -6,24 +6,35 @@ import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.jline.reader.*;
+import org.jline.reader.impl.history.DefaultHistory;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 public class ConsoleProgram {
 
     public static void main(String[] args)
             throws IOException, SqlParseException {
         while (true) {
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(System.in));
 
-            String input = reader.readLine();
+            Terminal terminal = TerminalBuilder.builder()
+                    .system(true)
+                    .build();
 
-            if (input.startsWith("SQL ")) {
+            LineReader lineReader = LineReaderBuilder.builder()
+                    .terminal(terminal)
+                    .history(new DefaultHistory())
+                    .build();
+
+            String input = lineReader.readLine().toLowerCase();
+            if (input.startsWith("sql-")) {
                 //1. SQL Parser
                 //2. Optimizer
                 String sqlQuery = input.substring(4);
@@ -55,6 +66,59 @@ public class ConsoleProgram {
 
                         int exitCode = executor.execute(cmdLine);
                         System.out.println(outputStream);
+                        break;
+                    case "run tpc-h":
+
+                        try (Stream<Path> paths = Files.walk(Paths.get("src/main/resources/tpc-h_queries"))) {
+
+
+                            String schemaPath = "src/main/resources/TPC-HTestDaten/CalciteSchema.json";
+                            CalciteOptimizer calciteOptimizer = new CalciteOptimizer(schemaPath);
+
+                            AtomicInteger queryCount = new AtomicInteger(1);
+
+                            paths.filter(Files::isRegularFile)
+                                    .forEach(queryPath -> {
+                                        System.out.println("TPC-H Query " + queryCount + ":\n");
+                                        queryCount.getAndIncrement();
+
+                                        //get file content
+                                        try {
+                                            String query = Files.readString(queryPath);
+                                            //filter out comments starting with : or -
+                                            String[] lines = query.split("\n");
+
+                                            StringBuilder stringBuilder = new StringBuilder();
+                                            for (String s:lines) {
+                                                if (!(s.startsWith(":") || s.startsWith("-"))) {
+                                                    s = s.replace(';', '\n');
+                                                    stringBuilder.append(s);
+                                                }
+                                            }
+
+                                            String sqlQuery = stringBuilder.toString();
+
+                                            System.out.println(calciteOptimizer.optimizeQuery(sqlQuery));
+
+                                        } catch (IOException e) {
+                                            System.err.println(e.getMessage());
+                                            throw new RuntimeException(e);
+                                        } catch (SqlParseException e) {
+                                            throw new RuntimeException(e);
+                                        }
+
+                                    });
+                        }
+
+
+
+
+
+
+
+
+                        //System.out.println(calciteOptimizer.optimizeQuery(sqlQuery).explain());
+
                         break;
                     default:
                         System.out.println("Unknown command: \"" + input + "\"");
